393e95919a96d8d8121d022f2c694c4bb114d046
Mateusz Witkowski <Mateusz.Witkowski@huntingdonshire.gov.uk>
2020-08-16 13:04:56 +0100

    Updates

---
+++ precache-manifest.66fb088d5a6695aeb68d99409fbe15a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13974a4d076d6e92fcb65828fde07e25",
+    "url": "/string-manipulator/index.html"
+  },
+  {
+    "revision": "2281a79158d40d78fc9a",
+    "url": "/string-manipulator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c54b5d304fd4fc32e889",
+    "url": "/string-manipulator/static/js/2.4b16adb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/string-manipulator/static/js/2.4b16adb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2281a79158d40d78fc9a",
+    "url": "/string-manipulator/static/js/main.d714da25.chunk.js"
+  },
+  {
+    "revision": "6e8b69e87eca29c441fb",
+    "url": "/string-manipulator/static/js/runtime-main.349b159b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/string-manipulator/static/media/logo.5d5d9eef.svg"
+  }
+]);
